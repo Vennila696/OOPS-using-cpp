747a7adfe4dfa3f79dff28da25e26d6f4a8088d6
Vennila Pitchaikannu <pvennila830@gmail.com>
2025-08-31 07:17:12 +0530

    Create Multiplication Function

---
+++ Functions/Multiplication Function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int multiply(int a, int b) {
+    return a * b;
+}
+int main() {
+    int num1, num2;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    int result = multiply(num1, num2);
+    cout << "Multiplication Result: " << result << endl;
+    return 0;
+}
